fd14428d570537957333ee6ffbc1cfd560de90c2
Fartuun Ali <fartuunali@Fartuuns-MacBook-Pro.local>
2022-08-23 20:11:14 -0500

    initial commit

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+// console.error('putDb not implemented');
+try {
+  const jateDB = await openDB('jate', 1);
+  const transaction = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({jate: content});
+  const result = await request;
+  console.log('Data was saved'+ result);
+  }catch(error) {console.error('putDb not implemented')}
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+try{
+  const jateDB = await opwnDB('jate', 1);
+  
+}
 
 initdb();
