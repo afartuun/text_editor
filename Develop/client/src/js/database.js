1372155c3c454d64c2eee07dedcef5eef274cb1a
Fartuun Ali <fartuunali@Fartuuns-MacBook-Pro.local>
2022-08-23 21:42:16 -0500

    finishing up the package json file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
 // console.error('putDb not implemented');
 try {
@@ -26,11 +26,18 @@ try {
 }
 
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-try{
+export const getDb = async () => {
+try {
   const jateDB = await opwnDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const effect = await request;
+  console.log(effect);
   
-}
+}catch (err) {
+  console.err('getDb not implemented')
+  }
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,26 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.style.display = inlineBlock; 
+});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+    if(!promptEvent){
+        return;
+    }
+    promptEvent.promtp();
+    window.defferedPrompt = null;
+    butInstall.style.display ="none";
+});
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    butInstall.style.display = "none";
+    window.defferedPrompt = null;
+});
